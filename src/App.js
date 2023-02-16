0f17d87d08f880b6da7eeb8efbf52fa3db03f0af
David Wagih <davidwagih62@gmail.com>
2023-02-16 19:19:34 +0200

    React-Router Lesson

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "../src/pages/AllMeetups";
+import NewMeetupPage from "../src/pages/NewMeetup";
+import FavoritesPage from "../src/pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  // localhost:3000
+  // my-page.com
+
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup" exact>
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites" exact>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
