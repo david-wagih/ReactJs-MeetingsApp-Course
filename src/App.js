0f63b85b28975c54034f3cfd7a3960266b650061
David Wagih <davidwagih62@gmail.com>
2023-02-23 15:54:52 +0200

    finsih the UI of meeting Form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "../src/pages/AllMeetups";
 import NewMeetupPage from "../src/pages/NewMeetup";
 import FavoritesPage from "../src/pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "../src/components/layout/Layout";
 
 function App() {
   // localhost:3000
@@ -11,18 +11,19 @@ function App() {
 
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup" exact>
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites" exact>
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup" exact>
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites" exact>
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
